5ec8fff9994eac0d7fde24f1644399877c1a81f2
EstebanP63 <palenciaesteban63@gmail.com>
2025-07-12 18:56:40 -0400

    Initial idea for lesson schema

--- server/models/Lesson.js
+++ server/models/Lesson.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose')
+
+
+const contentBlockSchema = new mongoose.Schema({
+  type: { type: String, required: true }, //  'text', 'table', 'question', etc.
+  order: { type: Number, required: true },
+
+  payload: { type: mongoose.Schema.Types.Mixed, required: true }
+
+  /* Payload schemas
+  
+  If (type === text) payload = string
+  If (type === table) payload = { headers: [String], rows: [[String]] }
+  If (type === question) payload = { text: String, answer: String }
+  
+  Maybe add a type for multiple choice questions
+  Maybe add images in the future
+    
+  */
+}, { _id: false })
+
 const lessonSchema = new mongoose.Schema({
   title:       { type: String, required: true },
-  content:     { type: String, required: true },
+  description: { type: String, required: true},
+  contentBlocks: [contentBlockSchema],
+  order: Number,
   author:      { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  prerequisites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' }],
+  nextLessonIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Lesson' }],
   modifiedBy:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 }, { timestamps: true })
 module.exports = mongoose.model('Lesson', lessonSchema)
